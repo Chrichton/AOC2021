92232d1ae5fc115c4cfb17026071cd2f6d61ffd9
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-11-30 22:15:49 +0100

    Day1 empty

---
+++ day1/test/day1_test.exs
@@ -0,0 +1,19 @@
+defmodule Day1Test do
+  use ExUnit.Case
+
+  # test "sample first star" do
+  #   assert Day1.solve1("sample1") == nil
+  # end
+
+  # test "first star" do
+  #   assert Day1.solve1("star1") == nil
+  # end
+
+  # test "sample second star" do
+  #   assert Day1.solve2("sample2") == nil
+  # end
+
+  # test "second star" do
+  #   assert Day1.solve2("star2") == nil
+  # end
+end
