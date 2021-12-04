01b403cef3efc8147b70f4efa66212477e15f944
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-04 10:32:05 +0100

    first step star1

---
+++ day4/lib/day4.ex
@@ -0,0 +1,128 @@
+defmodule Day4 do
+  def solve1(filename) do
+    {numbers, boards} =
+      filename
+      |> read_input()
+
+    find_first_bingo_board(numbers, 5, boards)
+    # find_first_bingo_board(numbers, 12, boards)
+  end
+
+  def find_first_bingo_board(numbers, index, _boards) when index > length(numbers), do: nil
+
+  def find_first_bingo_board(numbers, index, boards) do
+    actual_numbers = Enum.take(numbers, index)
+    winning_board = find_bingo_board(actual_numbers, boards)
+
+    IO.inspect(winning_board, label: "winning board")
+
+    if winning_board != [nil] do
+      hd(winning_board)
+    else
+      find_first_bingo_board(numbers, index + 1, boards)
+    end
+  end
+
+  def find_bingo_board(numbers, boards) do
+    transposed_boards =
+      boards
+      |> Enum.map(&transpose/1)
+
+    bingo_boards =
+      boards
+      |> Enum.flat_map(fn board ->
+        board
+        |> Enum.map(fn board_numbers ->
+          find_numbers_in_board_numbers(numbers, board_numbers)
+        end)
+      end)
+
+    if Enum.count(bingo_boards) > 0 do
+      Enum.take(bingo_boards, 1)
+    else
+      bingo_boards =
+        transposed_boards
+        |> Enum.flat_map(fn board ->
+          board
+          |> Enum.map(fn board_numbers ->
+            find_numbers_in_board_numbers(numbers, board_numbers)
+          end)
+        end)
+
+      if Enum.count(bingo_boards) > 0 do
+        Enum.take(bingo_boards, 1)
+      else
+        nil
+      end
+    end
+  end
+
+  def find_numbers_in_board_numbers(numbers, board_numbers) do
+    result =
+      board_numbers
+      |> Enum.all?(fn board_number ->
+        Enum.member?(numbers, board_number)
+      end)
+
+    if result == true do
+      board_numbers
+    else
+      nil
+    end
+  end
+
+  def transpose([]), do: []
+  def transpose([[] | _]), do: []
+
+  def transpose(a) do
+    [Enum.map(a, &hd/1) | transpose(Enum.map(a, &tl/1))]
+  end
+
+  def solve2(filename) do
+    filename
+    |> read_input()
+  end
+
+  def read_input(filename) do
+    data =
+      File.read!(filename)
+      |> String.split("\n")
+
+    numbers =
+      data
+      |> Enum.take(1)
+      |> Enum.at(0)
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+
+    boards =
+      data
+      |> Enum.drop(2)
+      |> build_boards([])
+
+    {numbers, boards}
+  end
+
+  def build_boards([], boards), do: boards
+
+  def build_boards(data, boards) do
+    board =
+      data
+      |> Enum.take_while(fn element -> element != "" end)
+      |> Enum.map(fn string ->
+        string
+        |> String.split(" ", trim: true)
+        |> Enum.map(fn number_string ->
+          number_string
+          |> String.to_integer()
+        end)
+      end)
+
+    new_data =
+      data
+      |> Enum.drop_while(fn element -> element != "" end)
+      |> Enum.drop(1)
+
+    build_boards(new_data, [board | boards])
+  end
+end
