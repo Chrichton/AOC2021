5c7db995b8e1d3d583105b66242f939439f4b81d
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-05 15:05:27 +0100

    points_in_line_hard -yes, points_in_line_easy -no

--- day5/lib/day5.ex
+++ day5/lib/day5.ex
@@ -4,6 +4,47 @@ defmodule Day5 do
     |> read_input()
   end
 
+  def points_in_line({fx, _fy} = from, {tx, _ty} = to) do
+    line = from_to(from, to)
+    # step = unit_vector(line)
+
+    slope = slope(from, to)
+    range = {fx + 1, tx}
+
+    require IEx
+    IEx.pry()
+
+    points_in_line([from], to, slope, range)
+  end
+
+  def points_in_line(from, _to, _slope, {x, to_x} = _range)
+      when is_list(from) and x > to_x,
+      do: from |> Enum.reverse()
+
+  def points_in_line([{lx, ly} = _last_value | _] = from, to, slope, {x, to_x} = _range)
+      when is_list(from) do
+    y =
+      (slope * (x - lx) + ly)
+      |> round()
+
+    require IEx
+    IEx.pry()
+
+    points_in_line([{x, y} | from], to, slope, {x + 1, to_x})
+  end
+
+  def slope({x1, y1} = _from, {x2, y2} = _to), do: (x2 - x1) / (y2 - y1)
+
+  def from_to({x1, y1}, {x2, y2}), do: {x2 - x1, y2 - y1}
+
+  def unit_vector({x, y} = v) do
+    v
+    |> norm()
+    |> then(fn norm -> {x / norm, y / norm} end)
+  end
+
+  def norm({x, y}), do: :math.sqrt(x * x + y * y)
+
   def solve2(filename) do
     filename
     |> read_input()
--- day5/test/day5_test.exs
+++ day5/test/day5_test.exs
@@ -1,19 +1,62 @@
 defmodule Day5Test do
   use ExUnit.Case
 
-  test "sample first star" do
-    assert Day5.solve1("sample1") == 7
+  test "norm" do
+    assert Day5.norm({2, 0}) == 2
   end
 
-  test "first star" do
-    assert Day5.solve1("star1") == 1387
+  test "unit_vector" do
+    assert Day5.unit_vector({2, 0}) == {1, 0}
+    assert Day5.unit_vector({0, 2}) == {0, 1}
+
+    actual =
+      {4, 5}
+      |> Day5.unit_vector()
+      |> Day5.norm()
+      |> Float.round()
+
+    assert actual == 1
   end
 
-  test "sample second star" do
-    assert Day5.solve2("sample1") == 5
+  test "from to" do
+    assert Day5.from_to({2, 0}, {6, 4}) == {4, 4}
   end
 
-  test "second star" do
-    assert Day5.solve2("star1") == 1362
+  test "points_in_line_easy" do
+    assert Day5.points_in_line({0, 1}, {0, 3}) == [
+             {0, 1},
+             {0, 2},
+             {0, 3}
+           ]
   end
+
+  test "points_in_line_hard" do
+    actual = Day5.points_in_line({2, 0}, {6, 4})
+
+    expected = [
+      {2, 0},
+      {3, 1},
+      {4, 2},
+      {5, 3},
+      {6, 4}
+    ]
+
+    assert actual == expected
+  end
+
+  # test "sample first star" do
+  #   assert Day5.solve1("sample1") == 5
+  # end
+
+  # test "first star" do
+  #   assert Day5.solve1("star1") == 1387
+  # end
+
+  # test "sample second star" do
+  #   assert Day5.solve2("sample1") == 5
+  # end
+
+  # test "second star" do
+  #   assert Day5.solve2("star1") == 1362
+  # end
 end
