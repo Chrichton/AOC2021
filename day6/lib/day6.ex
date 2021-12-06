72c4add6274eeba7bd2b291993ec639566722264
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-06 07:20:16 +0100

    first star

--- day6/lib/day6.ex
+++ day6/lib/day6.ex
@@ -2,6 +2,35 @@ defmodule Day6 do
   def solve1(filename) do
     filename
     |> read_input()
+    |> generate_lantern_fishs(80)
+    |> Enum.count()
+  end
+
+  def generate_lantern_fishs(lantern_fishs, 0), do: lantern_fishs
+
+  def generate_lantern_fishs(lantern_fishs, generations) do
+    next =
+      next_generation(lantern_fishs)
+      |> Enum.reverse()
+
+    generate_lantern_fishs(next, generations - 1)
+  end
+
+  def next_generation(lantern_fishs) do
+    lantern_fishs
+    |> Enum.reduce([], fn state, acc ->
+      {next_state, spawn_child} = next_state(state)
+
+      if spawn_child,
+        do: [8 | [next_state | acc]],
+        else: [next_state | acc]
+    end)
+  end
+
+  def next_state(state) do
+    if state == 0,
+      do: {6, true},
+      else: {state - 1, false}
   end
 
   def solve2(filename) do
@@ -11,7 +40,7 @@ defmodule Day6 do
 
   def read_input(filename) do
     File.read!(filename)
-    |> String.split("\n")
+    |> String.split(",")
     |> Enum.map(&String.to_integer/1)
   end
 end
--- day6/test/day6_test.exs
+++ day6/test/day6_test.exs
@@ -1,19 +1,34 @@
 defmodule Day6Test do
   use ExUnit.Case
 
+  test "generation4" do
+    actual =
+      Day6.generate_lantern_fishs(
+        [0, 0, 1, 5, 7, 6, 8],
+        1
+      )
+
+    # [0,    0,    1, 5, 7, 6, 8]
+    # [6, 8, 6, 8, 0, 4, 6, 5, 7]
+
+    expected = [6, 8, 6, 8, 0, 4, 6, 5, 7]
+
+    assert actual == expected
+  end
+
   test "sample first star" do
-    assert Day6.solve1("sample1") == 5
+    assert Day6.solve1("sample1") == 5934
   end
 
   test "first star" do
-    assert Day6.solve1("star1") == 1387
+    assert Day6.solve1("star1") == 365_862
   end
 
-  test "sample second star" do
-    assert Day6.solve2("sample1") == 5
-  end
+  # test "sample second star" do
+  #   assert Day6.solve2("sample1") == 5
+  # end
 
-  test "second star" do
-    assert Day6.solve2("star1") == 1362
-  end
+  # test "second star" do
+  #   assert Day6.solve2("star1") == 1362
+  # end
 end
