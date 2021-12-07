4bf10343124eaa67d2000dea0ce17e17151842fe
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-07 06:22:01 +0100

    first star

--- day7/lib/day7.ex
+++ day7/lib/day7.ex
@@ -2,6 +2,24 @@ defmodule Day7 do
   def solve1(filename) do
     filename
     |> read_input()
+    |> calculate_fuels()
+    |> Enum.min_by(fn {_key, count} -> count end)
+    |> elem(1)
+  end
+
+  def calculate_fuels(crabs) do
+    crabs
+    |> Enum.reduce([], fn crab_position, acc ->
+      fuel = move_all_crabs_to_position(crabs, crab_position)
+      [{crab_position, fuel} | acc]
+    end)
+  end
+
+  def move_all_crabs_to_position(crabs, position) do
+    crabs
+    |> Enum.reduce(0, fn crab_position, acc ->
+      abs(crab_position - position) + acc
+    end)
   end
 
   def solve2(filename) do
@@ -11,7 +29,7 @@ defmodule Day7 do
 
   def read_input(filename) do
     File.read!(filename)
-    |> String.split("\n", trim: true)
+    |> String.split(",", trim: true)
     |> Enum.map(&String.to_integer/1)
   end
 end
--- day7/test/day7_test.exs
+++ day7/test/day7_test.exs
@@ -2,13 +2,13 @@ defmodule Day7Test do
   use ExUnit.Case
   doctest Day7
 
-  # test "sample first star" do
-  #   assert Day7.solve1("sample1") == nil
-  # end
+  test "sample first star" do
+    assert Day7.solve1("sample1") == 37
+  end
 
-  # test "first star" do
-  #   assert Day7.solve1("star1") == nil
-  # end
+  test "first star" do
+    assert Day7.solve1("star1") == 352_997
+  end
 
   # test "sample second star" do
   #   assert Day7.solve2("sample1") == nil
