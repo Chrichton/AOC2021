13239ba63585f7f20bb5a85e55f50a296bd23515
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-07 05:40:04 +0100

    initial day7

---
+++ day7/test/day7_test.exs
@@ -0,0 +1,20 @@
+defmodule Day7Test do
+  use ExUnit.Case
+  doctest Day7
+
+  # test "sample first star" do
+  #   assert Day7.solve1("sample1") == nil
+  # end
+
+  # test "first star" do
+  #   assert Day7.solve1("star1") == nil
+  # end
+
+  # test "sample second star" do
+  #   assert Day7.solve2("sample1") == nil
+  # end
+
+  # test "second star" do
+  #   assert Day7.solve2("star1") == nil
+  # end
+end
