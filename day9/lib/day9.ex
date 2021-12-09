564846ff1e6cc2ac883fff10cf30f43106824887
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-09 05:51:01 +0100

    initial day9

---
+++ day9/lib/day9.ex
@@ -0,0 +1,16 @@
+defmodule Day9 do
+  def solve1(filename) do
+    filename
+    |> read_input()
+  end
+
+  def solve2(filename) do
+    filename
+    |> read_input()
+  end
+
+  def read_input(filename) do
+    filename
+    |> String.split("\n")
+  end
+end
---
+++ day9/test/day9_test.exs
@@ -0,0 +1,19 @@
+defmodule Day9Test do
+  use ExUnit.Case
+
+  test "sample first star" do
+    assert Day9.solve1("sample1") == nil
+  end
+
+  test "first star" do
+    assert Day9.solve1("star1") == nil
+  end
+
+  # test "sample second star" do
+  #   assert Day9.solve2("sample1") == nil
+  # end
+
+  # test "second star" do
+  #   assert Day9.solve2("star1") == nil
+  # end
+end
