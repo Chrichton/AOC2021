e13d67beccf424ad2cd608be27f5a52ab690dfe2
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-10 08:05:42 +0100

    solved both stars

--- day10/lib/day10.ex
+++ day10/lib/day10.ex
@@ -2,11 +2,88 @@ defmodule Day10 do
   def solve1(filename) do
     filename
     |> read_input()
+    |> Enum.filter(fn line ->
+      find_corrupted_char(line)
+      |> Enum.count() == 1
+    end)
+    |> Enum.map(fn corrupt_line ->
+      [char] = find_corrupted_char(corrupt_line)
+
+      case char do
+        ")" -> 3
+        "]" -> 57
+        "}" -> 1197
+        ">" -> 25137
+      end
+    end)
+    |> Enum.sum()
+  end
+
+  def chunk_delimiters do
+    %{
+      "(" => ")",
+      "[" => "]",
+      "{" => "}",
+      "<" => ">"
+    }
+  end
+
+  def find_corrupted_char(string) do
+    close_chars = Map.values(chunk_delimiters())
+
+    string
+    |> String.codepoints()
+    |> Enum.reduce_while([], fn char, acc ->
+      close_char = Map.get(chunk_delimiters(), char)
+
+      if close_char != nil do
+        {:cont, [close_char | acc]}
+      else
+        if char in close_chars do
+          [expected_close_char | tail] = acc
+
+          if char != expected_close_char do
+            {:halt, [char]}
+          else
+            {:cont, tail}
+          end
+        else
+          {:cont, acc}
+        end
+      end
+    end)
   end
 
   def solve2(filename) do
-    filename
-    |> read_input()
+    score_list =
+      filename
+      |> read_input()
+      |> Enum.reject(fn line ->
+        find_corrupted_char(line)
+        |> Enum.count() == 1
+      end)
+      |> Enum.map(fn corrupt_line ->
+        corrupt_closings = find_corrupted_char(corrupt_line)
+
+        Enum.map(corrupt_closings, fn closing ->
+          case closing do
+            ")" -> 1
+            "]" -> 2
+            "}" -> 3
+            ">" -> 4
+          end
+        end)
+      end)
+      |> Enum.map(fn scores ->
+        scores
+        |> Enum.reduce(0, fn score, acc ->
+          score + acc * 5
+        end)
+      end)
+      |> Enum.sort()
+
+    middle_index = (Enum.count(score_list) / 2) |> trunc()
+    Enum.at(score_list, middle_index)
   end
 
   def read_input(filename) do
--- day10/test/day10_test.exs
+++ day10/test/day10_test.exs
@@ -1,19 +1,27 @@
 defmodule Day10Test do
   use ExUnit.Case
 
+  test "find first corrupt character" do
+    assert Day10.find_corrupted_char("{([(<{}[<>[]}>{[]{[(<()>") == ["}"]
+    assert Day10.find_corrupted_char("[[<[([]))<([[{}[[()]]]") == [")"]
+
+    assert Day10.find_corrupted_char("<{([{{}}[<[[[<>{}]]]>[]]")
+           |> Enum.count() > 1
+  end
+
   test "sample first star" do
-    assert Day10.solve1("sample1") == nil
+    assert Day10.solve1("sample1") == 26397
   end
 
   test "first star" do
-    assert Day10.solve1("star1") == nil
+    assert Day10.solve1("star1") == 193_275
   end
 
   test "sample second star" do
-    assert Day10.solve2("sample1") == nil
+    assert Day10.solve2("sample1") == 288_957
   end
 
   test "second star" do
-    assert Day10.solve2("star1") == nil
+    assert Day10.solve2("star1") == 2_429_644_557
   end
 end
