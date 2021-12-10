8ba3387bad1947987ede16e567fe3ab0f7530f4e
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-10 18:29:29 +0100

    initial day11

---
+++ day11/test/day11_test.exs
@@ -0,0 +1,19 @@
+defmodule Day11Test do
+  use ExUnit.Case
+
+  test "sample first star" do
+    assert Day11.solve1("sample1") == 26397
+  end
+
+  # test "first star" do
+  #   assert Day11.solve1("star1") == 193_275
+  # end
+
+  # test "sample second star" do
+  #   assert Day11.solve2("sample1") == 288_957
+  # end
+
+  # test "second star" do
+  #   assert Day11.solve2("star1") == 2_429_644_557
+  # end
+end
