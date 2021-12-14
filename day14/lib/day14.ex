0020381a0543f893937a8819bc542f358649c667
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-14 07:58:32 +0100

    wip sample1

--- day14/lib/day14.ex
+++ day14/lib/day14.ex
@@ -2,6 +2,52 @@ defmodule Day14 do
   def solve1(filename) do
     filename
     |> read_input()
+    |> then(fn {template, rules} ->
+      build_polymer(template, rules)
+    end)
+    |> String.length()
+  end
+
+  def build_polymer(template, rules) do
+    1..10
+    |> Enum.reduce(template, fn _step, acc ->
+      build_polymer_step(acc, rules)
+    end)
+  end
+
+  def build_polymer_step(template, rules) do
+    template
+    |> all_pairs()
+    |> Enum.take(3)
+    |> Enum.reduce(template, fn key, acc ->
+      value = rules[key]
+
+      if value != nil do
+        first = String.first(key)
+        second = String.last(key)
+        replacement = first <> value <> second
+        result = String.replace(acc, key, replacement, global: false)
+
+        IO.inspect(acc, label: "acc")
+        IO.inspect(replacement, label: "replacement")
+        IO.inspect(result, label: "result")
+      else
+        acc
+      end
+    end)
+  end
+
+  def all_pairs(string) do
+    string
+    |> String.codepoints()
+    |> Enum.chunk_every(2, 1)
+    |> then(fn pairs ->
+      pairs
+      |> Enum.take(Enum.count(pairs) - 1)
+      |> Enum.map(fn [left, right] ->
+        left <> right
+      end)
+    end)
   end
 
   def solve2(filename) do
@@ -10,8 +56,20 @@ defmodule Day14 do
   end
 
   def read_input(filename) do
-    filename
-    |> File.read!()
-    |> String.split("\n")
+    [template | rules] =
+      filename
+      |> File.read!()
+      |> String.split("\n", trim: true)
+
+    rules =
+      rules
+      |> Enum.map(fn rule_string ->
+        String.split(rule_string, " -> ", trim: true)
+      end)
+      |> Enum.reduce(%{}, fn [source, target], acc ->
+        Map.put(acc, source, target)
+      end)
+
+    {template, rules}
   end
 end
--- day14/test/day14_test.exs
+++ day14/test/day14_test.exs
@@ -2,6 +2,20 @@ defmodule Day14Test do
   use ExUnit.Case
   doctest Day14
 
+  test "all_pairs" do
+    assert Day14.all_pairs("NNCB") == ["NN", "NC", "CB"]
+  end
+
+  test "build_polymer_step" do
+    actual =
+      Day14.read_input("sample1")
+      |> then(fn {template, rules} ->
+        Day14.build_polymer_step(template, rules)
+      end)
+
+    assert actual == "NCNBCHB"
+  end
+
   test "sample first star" do
     assert Day14.solve1("sample1") == 17
   end
