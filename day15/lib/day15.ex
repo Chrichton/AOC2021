451faa8d04d3f40d09ff6325b8f6b62ac7a96d1c
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-15 05:59:04 +0100

    Initial day15

---
+++ day15/lib/day15.ex
@@ -0,0 +1,17 @@
+defmodule Day15 do
+  def solve1(filename) do
+    filename
+    |> read_input()
+  end
+
+  def solve2(filename) do
+    filename
+    |> read_input()
+  end
+
+  def read_input(filename) do
+    filename
+    |> File.read!()
+    |> String.split("\n", trim: true)
+  end
+end
---
+++ day15/test/day15_test.exs
@@ -0,0 +1,19 @@
+defmodule Day15Test do
+  use ExUnit.Case
+
+  test "sample first star" do
+    assert Day15.solve1("sample1") == 17
+  end
+
+  test "first star" do
+    assert Day15.solve1("star1") == 695
+  end
+
+  test "sample secod star" do
+    assert Day15.solve2("sample1") == 17
+  end
+
+  test "second star" do
+    assert Day15.solve2("star1") == nil
+  end
+end
