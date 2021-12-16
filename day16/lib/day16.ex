be22c5255397412f7e966e07ab17b2c9ea30824b
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-16 09:03:13 +0100

    wip first sample

--- day16/lib/day16.ex
+++ day16/lib/day16.ex
@@ -2,6 +2,60 @@ defmodule Day16 do
   def solve1(filename) do
     filename
     |> read_input()
+    |> Enum.map(&decode_hex/1)
+    |> Enum.map(&decode_binary/1)
+  end
+
+  def decode_hex(hex_string) do
+    Base.decode16!(hex_string)
+    |> :binary.bin_to_list()
+    |> Enum.reduce("", fn number, acc ->
+      binary_string =
+        number
+        |> Integer.to_string(2)
+        |> String.pad_leading(8, "0")
+
+      acc <> binary_string
+    end)
+  end
+
+  def decode_binary(binary_string) do
+    packet_version =
+      binary_string
+      |> String.slice(3, String.length(binary_string))
+      |> String.slice(3, 3)
+      |> String.to_integer(2)
+
+    # packet_type_id =
+    #   binary_string
+    #   |> Enum.take(3)
+    #   |> String.to_integer(2)
+
+    # if packet_type_id == 4 do
+    #   decode_all_values(Enum.drop(binary_string, 6))
+    # else
+    # end
+  end
+
+  def decode_all_values(binary_string) do
+    binary_string
+    |> String.codepoints()
+    |> Enum.chunk_every(5)
+    |> Enum.map(&Enum.join/1)
+    |> Enum.reduce("", fn group, acc ->
+      if Enum.all?(String.codepoints(group), fn char -> char == "0" end) do
+        acc
+      else
+        acc <> String.slice(group, 1, String.length(group))
+      end
+    end)
+    |> String.to_integer(2)
+  end
+
+  def lsplit(string, char) do
+    if String.slice(string, 0, 1) == char,
+      do: lsplit(String.slice(string, 1, String.length(string)), char),
+      else: string
   end
 
   def solve2(filename) do
@@ -12,6 +66,17 @@ defmodule Day16 do
   def read_input(filename) do
     filename
     |> File.read!()
-    |> String.split("\n", trim: true)
+    |> String.codepoints()
+    |> Enum.chunk_every(6)
+    |> Enum.map(&Enum.join/1)
   end
 end
+
+"""
+["10111", "11110", "00101", "000"]
+10111 11110 00101 000
+
+
+01111110
+011111100101
+"""
--- day16/test/day16_test.exs
+++ day16/test/day16_test.exs
@@ -1,19 +1,37 @@
 defmodule Day16Test do
   use ExUnit.Case
 
+  test "lsplit" do
+    actual = Day16.lsplit("000123", "0")
+
+    assert actual == "123"
+  end
+
+  test "decode_all_values" do
+    actual = Day16.decode_all_values("101111111000101000")
+
+    assert actual == 2021
+  end
+
+  test "decode_hex" do
+    actual = Day16.decode_hex("D2FE28")
+
+    assert actual == "110100101111111000101000"
+  end
+
   test "sample first star" do
     assert Day16.solve1("sample1") == 17
   end
 
-  test "first star" do
-    assert Day16.solve1("star1") == 695
-  end
+  #   test "first star" do
+  #     assert Day16.solve1("star1") == 695
+  #   end
 
-  test "sample secod star" do
-    assert Day16.solve2("sample1") == 17
-  end
+  #   test "sample secod star" do
+  #     assert Day16.solve2("sample1") == 17
+  #   end
 
-  test "second star" do
-    assert Day16.solve2("star1") == nil
-  end
+  #   test "second star" do
+  #     assert Day16.solve2("star1") == nil
+  #   end
 end
