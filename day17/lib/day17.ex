7064f423ee9477d24e3315d90eaf03a00bf6d3ac
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-17 07:20:07 +0100

    finished parsing

--- day17/lib/day17.ex
+++ day17/lib/day17.ex
@@ -2,6 +2,26 @@ defmodule Day17 do
   def solve1(filename) do
     filename
     |> read_input()
+    |> input_to_map_set()
+  end
+
+  def input_to_map_set([{_x, {from_x, to_x}}, {_y, {from_y, to_y}}]) do
+    for x <- from_x..to_x do
+      for y <- from_y..to_y do
+        {x, y}
+      end
+    end
+    |> Enum.flat_map(fn x -> x end)
+    |> Enum.reduce(MapSet.new(), fn {_x, _y} = point, acc ->
+      MapSet.put(acc, point)
+    end)
+  end
+
+  def next_position({x, y} = _point, {vx, vy} = _velocity) do
+    next_position = {{x + vx}, {y + vy}}
+    next_velocity = {max(vx, vx - 1), vy - 1}
+
+    {next_position, next_velocity}
   end
 
   def solve2(filename) do
@@ -12,6 +32,16 @@ defmodule Day17 do
   def read_input(filename) do
     filename
     |> File.read!()
-    |> String.codepoints()
+    |> String.replace("target area: ", "")
+    |> String.split(",", trim: true)
+    |> Enum.map(fn position ->
+      String.split(position, "=", trim: true)
+      |> then(fn [coordinate, range] ->
+        [from_string, to_string] = String.split(range, "..", trim: true)
+        from = String.to_integer(from_string)
+        to = String.to_integer(to_string)
+        {coordinate, {from, to}}
+      end)
+    end)
   end
 end
--- day17/test/day17_test.exs
+++ day17/test/day17_test.exs
@@ -1,6 +1,21 @@
 defmodule Day17Test do
   use ExUnit.Case
 
+  test "read_input" do
+    actual = Day17.read_input("sample1")
+
+    assert actual == [{"x", {20, 30}}, {" y", {-10, -5}}]
+  end
+
+  test "input_to_map_set" do
+    actual =
+      [{"x", {0, 1}}, {"y", {2, 3}}]
+      |> Day17.input_to_map_set()
+      |> MapSet.to_list()
+
+    assert actual == [{0, 2}, {0, 3}, {1, 2}, {1, 3}]
+  end
+
   test "sample first star" do
     assert Day17.solve1("sample1") == 17
   end
