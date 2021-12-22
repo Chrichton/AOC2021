4f510942bb33ddc38cce296de6887cab24b8199f
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-22 16:01:53 +0100

    wip

--- day14/lib/day14.ex
+++ day14/lib/day14.ex
@@ -19,22 +19,51 @@ defmodule Day14 do
     template
     |> all_pairs()
     |> Enum.take(3)
-    |> Enum.reduce(template, fn key, acc ->
-      value = rules[key]
 
-      if value != nil do
-        first = String.first(key)
-        second = String.last(key)
-        replacement = first <> value <> second
-        result = String.replace(acc, key, replacement, global: false)
+    # |> Enum.reduce(template, fn key, acc ->
+    #   value = rules[key]
 
-        IO.inspect(acc, label: "acc")
-        IO.inspect(replacement, label: "replacement")
-        IO.inspect(result, label: "result")
-      else
-        acc
-      end
-    end)
+    #   if value != nil do
+    #     first = String.first(key)
+    #     second = String.last(key)
+    #     replacement = first <> value <> second
+    #     result = String.replace(acc, key, replacement, global: false)
+
+    #     IO.inspect(acc, label: "acc")
+    #     IO.inspect(replacement, label: "replacement")
+    #     IO.inspect(result, label: "result")
+    #   else
+    #     acc
+    #   end
+    # end)
+  end
+
+  def index_of(string, search_string) do
+    :binary.match(
+      string,
+      search_string
+      |> elem(0)
+    )
+  end
+
+  def replace_in_template(template, rules, [first_key, second_key, third_key]) do
+    first_index = index_of(template, first_key)
+    second_index = index_of(template, second_key)
+    third_index = index_of(template, third_key)
+
+    result =
+      String.slice(template, 0, first_index - 1) <>
+        rules[first_key] <> String.slice(template, first_index)
+
+    result =
+      String.slice(result, 0, second_index) <>
+        rules[second_key] <> String.slice(result, second_index + 1)
+
+    result =
+      String.slice(result, 0, third_index + 1) <>
+        rules[third_key] <> String.slice(result, third_index + 2)
+
+    result
   end
 
   def all_pairs(string) do
