21674decf91db33a358f99ab679958a64965ad4b
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-12-22 10:43:11 +0100

    initial day21

---
+++ day21/test/day21_test.exs
@@ -0,0 +1,19 @@
+defmodule Day21Test do
+  use ExUnit.Case
+
+  test "sample first star" do
+    assert Day21.solve1("sample1") == 17
+  end
+
+  test "first star" do
+    assert Day21.solve1("star1") == 695
+  end
+
+  test "sample secod star" do
+    assert Day21.solve2("sample1") == 17
+  end
+
+  test "second star" do
+    assert Day21.solve2("star1") == nil
+  end
+end
